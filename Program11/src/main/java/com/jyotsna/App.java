977014008fe70d81e124023145a94e03f62558e8
jyotsnagl <jyotsnag.amcec@gmail.com>
2026-06-23 15:47:43 +0530

    program 11

---
+++ Program11/src/main/java/com/jyotsna/App.java
@@ -0,0 +1,13 @@
+package com.jyotsna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
